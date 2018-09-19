4bd288952d66d93e35a4a208f526f623bf614c4e
Luke Dotson <lukedotson@gmail.com>
2018-09-18 23:20:36 -0500

    asf

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('static'));
 
-app.listen(3000, function () {
+app.listen(80, function () {
     console.log('App started on port 3000');
 });
