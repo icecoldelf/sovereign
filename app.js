e9cbbd1b3667ba87336324116532c596c6d72c74
Luke Dotson <lukedotson@gmail.com>
2019-01-26 12:46:59 -0600

    asf

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const GracefulShutdownManager = require('@moebius/http-graceful-shutdown').GracefulShutdownManager;
+
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/buck', (req, res) => res.send('Buck is amazing!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const server = app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+const shutdownManager = new GracefulShutdownManager(server);
+
+process.on('SIGTERM', () => {
+    shutdownManager.terminate(() => {
+        console.log('Server is gracefully terminated');
+    });
+});
 
 //luke's comment
