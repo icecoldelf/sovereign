cbba9c667fe988e8b1683078dbd24dfcdbaa2cd2
Luke Dotson <luke.dotson@KSD.io>
2019-01-28 13:44:54 -0600

    asfd

--- bot.js
+++ bot.js
@@ -1,34 +1,87 @@
-const postData = querystring.stringify({
-  'msg': 'Hello World!'
-});
+const tmi = require('tmi.js');
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
 
-const options = {
-  hostname: 'www.google.com',
-  port: 80,
-  path: '/upload',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': Buffer.byteLength(postData)
-  }
+var dynamodb = new AWS.DynamoDB();
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: "theenlightenedbot",
+    password: "oauth:lpsjzwbexroxgnw0j81uxfm8wq7feu"
+  },
+  channels: [
+    "theenlightenedbot",
+    "thefew",
+    "steelrain27"
+  ]
 };
 
-const req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
-});
+// Create a client with our options
+const client = new tmi.client(opts);
 
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
-});
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
 
-// write data to request body
-req.write(postData);
-req.end();
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+  const userType = context["user-type"] || "none";
+
+  // Save message in DynamoDB
+  var params = {
+    Item: {
+     "timestamp": {
+       N: Date.now().toString()
+     },
+     "display-name": {
+       S: context["display-name"]
+     },
+     "message": {
+       S: commandName
+     },
+     "channel": {
+       S: target
+     },
+     "subscriber": {
+       BOOL: context.subscriber
+     },
+     "user-type": {
+       S: userType
+     }
+    }, 
+    ReturnConsumedCapacity: "TOTAL", 
+    TableName: "chat"
+   };
+   dynamodb.putItem(params, function(err, data) {
+     if (err) console.log(err, err.stack); // an error occurred
+     else     console.log(data);           // successful response
+   });
+
+  // If the command is known, let's execute it
+  if (commandName === '!diceasdf') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Function called when the "dice" command is issued 
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
