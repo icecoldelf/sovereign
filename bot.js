f30d7e2f2667fc21e25a25c58be88a4a566f0d10
Luke Dotson <lukedotson@gmail.com>
2019-02-03 16:29:35 -0600

    asfd

--- bot.js
+++ bot.js
@@ -100,7 +100,7 @@ function onMessageHandler (target, context, msg, self) {
     account = new bank.Account(response => client.say(target, response.happy));
   } else if (commandName === '!balance') {
     new bank.Account(account => {
-      account.accountNumber = "asfd";
+      account.accountNumber = context["user-id"];
       account.getBalance(response => client.say(target, response));
     });
   } else if (commandName === '!diceasdf') {
