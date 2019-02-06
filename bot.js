40f0d01da12655f878ae03f412ec97737f845dac
Luke Dotson <lukedotson@gmail.com>
2019-02-05 20:47:00 -0600

    adf

--- bot.js
+++ bot.js
@@ -99,6 +99,7 @@ function onMessageHandler (target, context, msg, self) {
 
   commandArray = commandName.split(' ');
   if (commandArray[0] != '!sov') commandArray = [];
+  console.log("commandArray[1]: " + commandArray[1]);
 
   switch(commandArray[1]) {
     case 'balance': checkBalance(commandArray, context, response => client.say(target, response));
